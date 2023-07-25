dff0540e5d9fc5631d63c04b67024b789fd30976
Sankaralingam Seranthian <sankaralingam.psg@gmail.com>
2023-07-25 19:56:39 +0200

    update

--- dist/pricinganalytics/pricinganalytics.esm.js
+++
@@ -1,2 +0,0 @@
-import{p as e,b as t}from"./p-e0808ed2.js";export{s as setNonce}from"./p-e0808ed2.js";const p=()=>{const s=import.meta.url;const t={};if(s!==""){t.resourcesUrl=new URL(".",s).href}return e(t)};p().then((e=>t([["p-e55dbe4a",[[1,"pricing-analytics",{first:[1],middle:[1],last:[1],pKey:[1,"p-key"]}]]]],e)));
-//# sourceMappingURL=pricinganalytics.esm.js.map
